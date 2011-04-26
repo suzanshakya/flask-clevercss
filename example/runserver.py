1c49767214fc7548a1d11fdd93e46e9a09e9bdcc
Sujan Shakya <suzan@immune.dk>
2011-04-27 04:57:05 +0545

    removed local import of flaskext.clevercss

--- example/runserver.py
+++ example/runserver.py
@@ -1,17 +1,13 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.debug = True
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+app.debug = True
 if app.debug:
-    import sys
-    sys.stdout = sys.stderr
-    sys.path.insert(0, '..')
-    del sys.modules['flaskext']
     from flaskext.clevercss import clevercss
     clevercss(app)
 
